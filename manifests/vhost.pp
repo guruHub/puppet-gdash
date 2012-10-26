14d9580634fddca0d5f3c5d2dea881cef58437d8
Guzmán Brasó <guzman@guruhub.com.uy>
2012-10-26 16:11:06 -0200

    Added support for custom dashboard title and filters

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -12,7 +12,7 @@ class gdash::vhost(
   if $redirect_home_to {
 	exec { "${vhost}-check-modrewrite-enable" :
 		path => '/usr/bin/:/bin:/usr/sbin',
-		command => 'a2enmod rewrite && /etc/init.d/apache reload',
+		command => 'a2enmod rewrite >> /dev/null && /etc/init.d/apache2 reload',
 		unless  => 'test -L /etc/apache2/mods-enabled/rewrite.load',
 		before  => File["/etc/${service}/conf.d/${vhost}.conf"]
 	}	
